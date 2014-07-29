4a1ef2c729be1135071c0360a227607551f0d624
jumblesale <rob@robsollars.co.uk>
2014-07-29 10:29:04 +0100

    Removed unused dependencies from the AppKernel

--- test/behat/Fixtures/TestApp/app/AppKernel.php
+++ test/behat/Fixtures/TestApp/app/AppKernel.php
@@ -9,22 +9,12 @@ class AppKernel extends Kernel
     {
         $bundles = array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
             new OldSound\RabbitMqBundle\OldSoundRabbitMqBundle(),
-            //new Symfony\Bundle\MonologBundle\MonologBundle(),
-            //new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            //new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            //new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
-            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Lrqdo\Bundle\RabbitMqSynchronizerBundle\LrqdoRabbitMqSynchronizerBundle();
-            //$bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            //$bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
 
         return $bundles;
